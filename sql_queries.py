cd40ec98a2dbc1feef9590547af9712d341636c3
Michael Eby <michaeleby1@gmail.com>
2020-05-14 16:04:44 -0400

    created tables, tried to run etl but dwhuser is not authorized to assume IAM Role

--- sql_queries.py
+++ sql_queries.py
@@ -72,7 +72,7 @@ songplay_table_create = ("""
         )
 """)
 
-user_table_create = ("""
+user_table_create = """
     CREATE TABLE IF NOT EXISTS
         users (
             user_id INT PRIMARY KEY DISTKEY,
@@ -81,7 +81,7 @@ user_table_create = ("""
             gender VARCHAR(1),
             level VARCHAR(4)           
         )
-""")
+"""
 
 song_table_create = ("""
     CREATE TABLE IF NOT EXISTS
@@ -133,29 +133,171 @@ staging_events_copy = f"""
       {config['S3']['LOG_JSONPATH']}
 """
 
-staging_songs_copy = ("""
-""").format()
+staging_songs_copy = f"""
+    COPY
+      staging_songs
+    FROM
+      {config['S3']['SONG_DATA']}
+    CREDENTIALS
+      'aws_iam_role={config['IAM_ROLE']['ARN']}'
+    REGION
+      'us-west-2'
+    JSON
+      'auto'
+"""
 
 # FINAL TABLES
 
-songplay_table_insert = ("""
-""")
+songplay_table_insert = """
+    INSERT INTO
+        songplays (
+            start_time,
+            user_id,
+            level,
+            song_id,
+            artist_id,
+            session_id,
+            location,
+            user_agent
+      )
+    SELECT
+        TIMESTAMP 'epoch' + e.timestamp * INTERVAL '1 second' as start_time,
+            e.user_id,
+            e.level,
+            s.song_id,
+            s.artist_id,
+            e.session_id,
+            e.location,
+            e.user_agent
+    FROM
+        staging_events e
+    JOIN
+        staging_songs s
+    ON
+        e.artist = s.artist_name AND
+        e.length = s.duration AND
+        e.song = s.title
+    WHERE
+        e.page = 'NextSong'
+"""
 
-user_table_insert = ("""
-""")
+user_table_insert = """
+    INSERT INTO
+        users (
+            user_id,
+            first_name,
+            last_name,
+            gender,
+            level
+        )
+    SELECT
+        DISTINCT user_id,
+        first_name,
+        last_name,
+        gender,
+        level
+    FROM
+        staging_events
+    WHERE
+        page = 'NextSong'
+"""
 
-song_table_insert = ("""
-""")
+song_table_insert = """
+    INSERT INTO
+        songs (
+            song_id,
+            title,
+            artist_id,
+            year,
+            duration
+        )
+    SELECT
+        DISTINCT song_id,
+        title,
+        artist_id,
+        year,
+        duration
+    FROM
+        staging_songs
+"""
 
-artist_table_insert = ("""
-""")
+artist_table_insert = """
+    INSERT INTO
+        artists (
+            artist_id,
+            name,
+            location,
+            latitude,
+            longitude
+      )
+    SELECT
+        DISTINCT artist_id,
+        artist_name,
+        artist_location,
+        artist_latitude,
+        artist_longitude
+    FROM
+        staging_songs
+"""
 
-time_table_insert = ("""
-""")
+time_table_insert = """
+    INSERT INTO
+        time (
+            start_time,
+            hour,
+            day,
+            week_of_year,
+            month,
+            year,
+            weekday
+      )
+    SELECT
+        start_time,
+        EXTRACT(HOUR FROM start_time) AS hour,
+        EXTRACT(DAY FROM start_time) AS day,
+        EXTRACT(WEEK FROM start_time) AS week,
+        EXTRACT(MONTH FROM start_time) AS month,
+        EXTRACT(YEAR FROM start_time) AS year,
+        CASE WHEN EXTRACT(DAY FROM start_time) IN (6, 7) THEN false ELSE true
+            END AS weekday
+    FROM (
+        SELECT
+            TIMESTAMP 'epoch' + timestamp * INTERVAL '1 second' as start_time
+        FROM
+            staging_events
+      ) s
+"""
 
 # QUERY LISTS
 
-create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
-copy_table_queries = [staging_events_copy, staging_songs_copy]
-insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
+### QUERY LISTS ###
+
+create_table_queries = [
+    staging_events_table_create,
+    staging_songs_table_create,
+    artist_table_create,
+    user_table_create,
+    song_table_create,
+    time_table_create,
+    songplay_table_create
+]
+drop_table_queries = [
+    staging_events_table_drop,
+    staging_songs_table_drop,
+    songplay_table_drop,
+    user_table_drop,
+    song_table_drop,
+    artist_table_drop,
+    time_table_drop
+]
+copy_table_queries = [
+    staging_events_copy,
+    staging_songs_copy
+]
+insert_table_queries = [
+    songplay_table_insert,
+    user_table_insert,
+    song_table_insert,
+    artist_table_insert,
+    time_table_insert
+]
